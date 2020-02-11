840d2e9505a79fc992a420abad925fa964ca7f21
Leopoldo Abing <abing-leopoldo@hpi.local>
2020-02-11 17:07:57 +0800

    episode 4.2

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -1,10 +1,16 @@
 <h1>Customers</h1>
 
 
-<ul>
+<!-- <ul>
   <?php
         foreach ($customers as $customer) {
           echo '<li>' . $customer . '</li>';
         }
   ?>
+</ul> -->
+
+<ul>
+  @foreach ($customers as $customer)
+        <li>{{$customer}}</li>
+  @endforeach
 </ul>
