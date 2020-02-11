6462792eccc8302272894c36ee5de56667704f2f
Leopoldo Abing <abing-leopoldo@hpi.local>
2020-02-11 17:13:03 +0800

    ep 5 controllers

---
+++ app/Http/Controllers/CustomersController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomersController extends Controller
+{
+    public function list(){
+        $customers = [
+            'John Doe',
+            'Jane Doe',
+            'Bob',
+            'Another Name'
+        ];
+    
+        return view('internals.customers', 
+            ['customers' => $customers, 
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,4 @@ Route::view('/', 'welcome');
 Route::view('contact-us', 'contact');
 Route::view('about', 'about');
 
-
-Route::get('customers', function(){
-
-    $customers = [
-        'John Doe',
-        'Jane Doe',
-        'Bob',
-    ];
-
-    return view('internals.customers', 
-        ['customers' => $customers, 
-    ]);
-});
+Route::get('customers', 'CustomersController@list');
