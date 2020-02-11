fe92d6bb07b054f133ee138b52237bb3d6d91111
Leopoldo Abing <abing-leopoldo@hpi.local>
2020-02-11 18:00:17 +0800

    ep7.1

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,18 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
 
 class CustomersController extends Controller
 {
     public function list(){
-        $customers = [
-            'John Doe',
-            'Jane Doe',
-            'Bob',
-            'Another Name'
-        ];
-    
+        $customers = Customer::all();
         return view('internals.customers', 
             ['customers' => $customers, 
         ]);
