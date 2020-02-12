40f6fa035489417126a4cfbaa9247942734e3e48
Leopoldo Abing <abing-leopoldo@hpi.local>
2020-02-12 17:44:26 +0800

    ep13 guarded field variable

--- app/Customer.php
+++ app/Customer.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
 
-    protected $fillable = ['name', 'email', 'active'];
+    // fillable Field Example
+    // protected $fillable = ['name', 'email', 'active'];
+
+    //Guerded Field
+    // ex. if you want to not allowed the email field add on the guearded field array variable
+    protected $guarded = [];
 
     public function scopeActive($query)
     {
