4b307a58b6fad25848439a8b05311d022579d8ea
Leopoldo Abing <abing-leopoldo@hpi.local>
2020-02-12 17:29:15 +0800

    ep13 Make Scoped function

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+
+    public function scopeActive($query)
+    {
+        return $query->where('active', 1);
+    }
+
+    public function scopeInactive($query)
+    {
+        return $query->where('active', 0);
+    }
 }
--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -10,8 +10,8 @@ class CustomersController extends Controller
     public function list()
     {
         
-        $activeCustomers = Customer::where('active',1)->get();
-        $inactiveCustomers = Customer::where('active',0)->get();
+        $activeCustomers = Customer::active()->get();
+        $inactiveCustomers = Customer::inactive()->get();
 
         return view(
             'internals.customers', compact('activeCustomers','inactiveCustomers')
