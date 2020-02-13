d5d5544979146beddab2011fda348d8aa4901a88
Leopoldo Abing <abing-leopoldo@hpi.local>
2020-02-13 17:36:33 +0800

    Update Home for Testing Heroku

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,5 @@
 
 @section('content')
         <h1>Welcome to Laravel 5.8</h1>
-        <h1>Welcome to Laravel 5.8</h1>
-        <h1>Welcome to Laravel 5.8</h1>
-        <h1>Welcome to Laravel 5.8</h1>
-        <h1>Welcome to Laravel 5.8</h1>
-        <h1>Welcome to Laravel 5.8</h1>
-        <h1>Welcome to Laravel 5.8</h1>
+     
 @endsection
